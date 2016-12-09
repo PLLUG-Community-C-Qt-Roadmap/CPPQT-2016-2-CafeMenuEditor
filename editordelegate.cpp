858660aeabe6e57976a752d5906a3dac9ba3eda6
Діма Майкл < >
2016-12-09 23:45:53 +0200

    Task 3. Implemented Menu editor delegate

--- editordelegate.cpp
+++ editordelegate.cpp
@@ -9,6 +9,7 @@ EditorDelegate::EditorDelegate(QWidget *parent) :
     ui(new Ui::EditorDelegate)
 {
     ui->setupUi(this);
+    connect(ui->menuNameLineEdit, SIGNAL(textChanged(QString)), this, SIGNAL(itemChanged()), Qt::UniqueConnection);
     connect(ui->menuItemNameLineEdit, SIGNAL(textChanged(QString)), this, SIGNAL(itemChanged()), Qt::UniqueConnection);
     connect(ui->menuItemDescriptionLineEdit, SIGNAL(textChanged(QString)), this, SIGNAL(itemChanged()), Qt::UniqueConnection);
     connect(ui->menuItemPriceSpinBox, SIGNAL(valueChanged(double)), this, SIGNAL(itemChanged()), Qt::UniqueConnection);
@@ -42,6 +43,8 @@ void EditorDelegate::visit(Menu *menu)
     clear();
 
     ui->stackedWidget->setCurrentWidget(ui->pageMenu);
+    ui->menuNameLineEdit->setText(menu->title().c_str());
+    ui->menuCountLabel->setText(QString::number(menu->count()));
 
     mEditedMenu = menu;
 
@@ -61,5 +64,9 @@ void EditorDelegate::slotSave()
         mEditedMenuItem->setTitle(ui->menuItemNameLineEdit->text().toStdString());
         mEditedMenuItem->setDescription(ui->menuItemDescriptionLineEdit->text().toStdString());
         mEditedMenuItem->setPrice(ui->menuItemPriceSpinBox->value());
+    }
+    if(mEditedMenu)
+    {
+        mEditedMenu->setTitle(ui->menuNameLineEdit->text().toStdString());
     }
 }
